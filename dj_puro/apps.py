a61ce6ef63735f9a1862131a9d31181796133f54
jonathan <jonamaita@gmail.com>
2020-12-27 18:31:35 -0300

    feat: add verbose_name in DjPuroConfig class in dj_puro/apps.py

--- dj_puro/apps.py
+++ dj_puro/apps.py
@@ -1,5 +1,14 @@
+"""
+dj_puro aplication module
+"""
+
 from django.apps import AppConfig
 
 
 class DjPuroConfig(AppConfig):
+    """
+    dj_puro application settings
+    """
+
     name = 'dj_puro'
+    verbose_name = "Django_Puro"
