8feb438dbb6580ce705dc468fe1cdcaeef087152
jonathan <jonamaita@gmail.com>
2020-12-27 16:46:42 -0300

    feat: add category_list view and category_detail view in dj_puro/views.py

--- dj_puro/views.py
+++ dj_puro/views.py
@@ -1,12 +1,35 @@
 """
     Views to dj_puro app
 """
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
+from .models import Category
 
 
 def category_list(request):
-    pass
+    """
+    Category list view
+    """
+    max_objects = 20
+    cat = Category.objects.all()[:max_objects]
+    data = {"results": list(cat.values("description", "active"))}
+
+    return JsonResponse(data)
 
 
-def category_detail(request):
-    pass
+def category_detail(request, pk):
+    """
+    Category detail view
+    """
+    # pylint: disable=invalid-name
+
+    cat = get_object_or_404(Category, pk=pk)
+    data = {
+        "results": {
+            "description": cat.description,
+            "active": cat.active,
+        }
+    }
+
+    return JsonResponse(data)
