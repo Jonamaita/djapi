014ed755b6cc977fa5c1f98680a5b85358ffb29b
jonathan <jonamaita@gmail.com>
2020-12-27 18:30:33 -0300

    feat: register Categroy model in admin

--- dj_puro/admin.py
+++ dj_puro/admin.py
@@ -1,3 +1,14 @@
+"""
+Categroy admin classes.
+"""
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category admin class
+    """
+
+    list_display = ("description", "active")
