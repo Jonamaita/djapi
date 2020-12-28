263be2edbbe508fc214650b8a9a8e9ad3d80d247
jonathan <jonamaita@gmail.com>
2020-12-27 22:40:45 -0300

    feat: add the api app models  to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,32 @@
+"""
+Categroy admin classes.
+"""
 from django.contrib import admin
+from .models import Category, SubCategory, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category admin class
+    """
+
+    list_display = ("description",)
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    """
+    SubCategory Admin class
+    """
+
+    list_display = ("category", "description")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Product Admin class
+    """
+
+    list_display = ("subcategory", "description", "sold")
