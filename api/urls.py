94871e6a5e16e4d5d8119b8819cac6dcc8a437e2
jonathan <jonamaita@gmail.com>
2021-01-03 20:38:13 -0300

    feat: add category and subcategory path in api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,17 +2,28 @@
 urls for api app
 """
 from django.urls import path
-from .views import ProductList, ProductDetail
+
+from .views import CategorySave, ProductDetail, ProductList, SubCategorySave
 
 urlpatterns = [
     path(
         "v1/products/",
         ProductList.as_view(),
-        name="product_list"
+        name="product_list",
     ),
     path(
         "v1/product/<int:pk>",
         ProductDetail.as_view(),
-        name="product_detail"
+        name="product_detail",
+    ),
+    path(
+        "v1/category/",
+        CategorySave.as_view(),
+        name="category_save",
+    ),
+    path(
+        "v1/subcategory/",
+        SubCategorySave.as_view(),
+        name="subcategory_save",
     ),
 ]
