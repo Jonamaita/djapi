be2d537d26f3ba445decf4b6c7d5249837b01f8a
jonathan <jonamaita@gmail.com>
2021-01-10 19:39:15 -0300

    feat: add path to LoginView view in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,14 @@
 Urls for users app
 """
 from django.urls import path
-from users.views import UserCreate
+
+from users.views import LoginView, UserCreate
 
 urlpatterns = [
     path(
         "create/",
         UserCreate.as_view(),
         name="user_create",
-    )
+    ),
+    path("login/", LoginView.as_view(), name="login"),
 ]
