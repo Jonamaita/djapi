a805bffe434cc9b58216481cc9edb16872476d01
jonathan <jonamaita@gmail.com>
2021-01-10 19:40:40 -0300

    feat: add LoginView class in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 """
 views for users app
 """
-from rest_framework import generics
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
 
@@ -14,3 +17,29 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    """
+    Manual Login
+    """
+
+    permission_classes = ()
+
+    def post(self, request):
+        """
+        Post request
+        """
+        username = self.request.data.get("username")
+        password = self.request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user:
+            return Response(
+                {"token": user.auth_token.key},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"error": "Bad credentials"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
