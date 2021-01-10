65f0c0bac0800f1382d9974c8022b2878be91521
jonathan <jonamaita@gmail.com>
2021-01-10 19:45:48 -0300

    feat: add auth view drf in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,13 +3,14 @@ Urls for users app
 """
 from django.urls import path
 
+# Vista de autenticación por defecto de Drf
+from rest_framework.authtoken import views
+
 from users.views import LoginView, UserCreate
 
 urlpatterns = [
-    path(
-        "create/",
-        UserCreate.as_view(),
-        name="user_create",
-    ),
+    path("create/", UserCreate.as_view(), name="user_create"),
     path("login/", LoginView.as_view(), name="login"),
+    # Autenticación con la vista por defecto de Drf
+    path("login-drf/", views.obtain_auth_token, name="login-drf"),
 ]
